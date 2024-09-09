b327248eb9d57ef71cdd67c1132e856a93de0ec4
tristan-gscn <tristan.gcsn@gmail.com>
2024-09-09 19:52:44 +0200

    Update app.py for web structure

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+#Route HTML files
 @app.route('/')
-def home():
-    return "Hello, Flask!"
+def route_landing_page():
+    return send_from_directory('web/html', 'landing-page.html')
+
+# Route CSS files
+@app.route('/<filename>.css')
+def route_css(filename):
+    return send_from_directory('web/css', filename + '.css')
+#Route JS files
+@app.route('/<filename>.js')
+def route_js(filename):
+    return send_from_directory('web/js', filename + '.js')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
