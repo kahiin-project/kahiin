fd69fde1fe27a28ec359f62024d69d7da03fa9fe
Mathieu_cpd <mathieu.chapouand@gmail.com>
2024-09-10 22:24:24 +0200

    Create the user list

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import json
 import sys
@@ -14,6 +14,18 @@ socketio = SocketIO(app)
 app.static_folder = 'web/static'
 app.template_folder = 'web/templates'
 
+clients = []
+
+
+class Client:
+    def __init__(self, sid, username):
+        self.sid = sid
+        self.username = username
+        clients.append(self)
+
+    def __del__(self):
+        clients.remove(self)
+
 
 @app.route('/admin')
 def route_admin():
@@ -23,6 +35,36 @@ def route_admin():
 @socketio.on('message')
 def handle_message(message):
     emit('message', message)
+
+
+@app.route('/homepage')
+def route_homepage():
+    return render_template('homepage.html')
+
+
+@socketio.on('addUser')
+def handle_connect(username):
+    if any(c.username == username for c in clients):
+        emit('error', 'Nom d\'utilisateur déjà pris')
+        return
+    elif any(c.sid == request.sid for c in clients):
+        emit('error', 'Vous êtes déjà connecté')
+    for c in clients:
+        emit('newUser', {'username': c.username, 'sid': c.sid})
+    session = Client(request.sid, username)
+    emit('newUser', {'username': session.username,
+         'id': session.sid}, broadcast=True)
+
+
+@socketio.on('edit')
+def handle_edit(message: dict):
+    if message['passcode'] == passcode:
+        config[message['key']] = message['value']
+        with open('config.json', 'w') as config_file:
+            json.dump(config, config_file)
+        emit('edit', message)
+    else:
+        emit('error', 'Invalid passcode')
 
 
 @app.route('/')
