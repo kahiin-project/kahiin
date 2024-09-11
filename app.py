6cfab45dc360b231bcd1c3c9a4454d326af0637b
Mathieu_cpd <mathieu_cpd@noreply.codeberg.org>
2024-09-11 09:22:26 +0200

    Create display page and user removal

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask_socketio import SocketIO, emit
 import json
 import sys
 
-passcode = sys.argv[1]
-
+#passcode = sys.argv[1]
+passcode = 'a'
 with open('config.json') as config_file:
     config = json.load(config_file)
 
@@ -26,6 +26,16 @@ class Client:
     def __del__(self):
         clients.remove(self)
 
+display_list = []
+
+class Display:
+    def __init__(self, sid, code):
+        self.sid = sid
+        self.code = code
+        display_list.append(self)
+    
+    def __del__(self):
+        display_list.remove(self)
 
 @app.route('/admin')
 def route_admin():
@@ -41,19 +51,44 @@ def handle_message(message):
 def route_homepage():
     return render_template('homepage.html')
 
+@app.route('/display')
+def route_display_page():
+    return render_template('display-page.html')
+
+@socketio.on('displayConnect')
+def handle_display_connect(code):
+    if code == passcode:
+        session = Display(request.sid, code)
+        for c in clients:
+            emit('newUser', {'username': c.username, 'sid': c.sid})
+    else:
+        emit('error', "Vous n'avez pas entré le bon passcode")
 
 @socketio.on('addUser')
 def handle_connect(username):
+    sessid = request.sid
     if any(c.username == username for c in clients):
         emit('error', 'Nom d\'utilisateur déjà pris')
         return
-    elif any(c.sid == request.sid for c in clients):
+    elif any(c.sid == sessid for c in clients):
         emit('error', 'Vous êtes déjà connecté')
-    for c in clients:
-        emit('newUser', {'username': c.username, 'sid': c.sid})
-    session = Client(request.sid, username)
+    session = Client(sessid, username)
     emit('newUser', {'username': session.username,
          'id': session.sid}, broadcast=True)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    sessid = request.sid
+    for c in clients:
+        if c.sid == sessid:
+            for d in display_list:
+                emit('rmUser', {'username' : c.username, 'passcode' : passcode}, to=d.sid)
+                del(c)
+                return
+
+    for d in display_list:
+        if d.sid == sessid:
+            del(d)
 
 
 @socketio.on('edit')
@@ -71,6 +106,5 @@ def handle_edit(message: dict):
 def route_landing_page():
     return render_template('landing-page.html')
 
-
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=8080)
+    socketio.run(app, debug=True, port=8080, host="0.0.0.0")
---
+++ web/static/display-page.js
@@ -0,0 +1,33 @@
+function Submit() {
+  const socket = io();
+  const passcode = document.getElementById("passcode").value;
+  
+  socket.emit("displayConnect", passcode);
+  document.getElementById("form").style.display = "none";
+  document.getElementById("list").style.display = "block";
+
+  socket.on("newUser", (res) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(res.username));
+    document.getElementById("users").appendChild(li);
+  });
+
+  socket.on("rmUser", (res) => {
+    if(res.passcode == passcode) {
+      const usersList = document.getElementById("users");
+      const items = usersList.getElementsByTagName("li");
+      for (let i = 0; i < items.length; i++) {
+        if (items[i].textContent === res.username) {
+          usersList.removeChild(items[i]);
+          break;
+        }
+      }
+    }
+  });
+
+  socket.on("error", (res) => {
+    alert(res);
+    document.getElementById("list").style.display = "none";
+    document.getElementById("form").style.display = "block";
+  });
+}
