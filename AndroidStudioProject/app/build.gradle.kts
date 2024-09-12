adf9bb77d66c639cd27512cd8f6b5ae154fcb6ee
tristan-gscn <tristan.gcsn@gmail.com>
2024-09-12 22:31:03 +0200

    Use Termux API in Android app

--- AndroidStudioProject/app/build.gradle.kts
+++ AndroidStudioProject/app/build.gradle.kts
@@ -51,6 +51,10 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.concurrent.futures)
+    implementation(libs.guava)
+
+    implementation(libs.listenablefuture)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- AndroidStudioProject/app/src/main/java/org/codeberg/kahiin/MainActivity.kt
+++ AndroidStudioProject/app/src/main/java/org/codeberg/kahiin/MainActivity.kt
@@ -1,11 +1,36 @@
 package org.codeberg.kahiin
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val commands = """
+//            cd ~
+//            rm -r kahiin
+//            pkg install git
+//            git clone https://codeberg.org/tristan-gscn/kahiin.git
+//            cd kahiin
+//            ./start.sh
+//        """.trimIndent()
+        val commands = "ls"
+
+        val intent = Intent("com.termux.api.RUN_COMMAND").apply {
+            putExtra("command", commands)
+        }
+
+        Log.d("MainActivity", "Intent created: $intent")
+
+        try {
+            startActivity(intent)
+            Log.d("MainActivity", "Intent started successfully")
+        } catch (e: Exception) {
+            Log.e("MainActivity", "Failed to start intent", e)
+        }
     }
 }
