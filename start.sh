a89b755857e17dfe74cb3de0921ac44336dfd783
Mathieu_cpd <mathieu.chapouand@gmail.com>
2024-09-18 18:14:40 +0200

    Update start.sh script to support Arch Linux-based distributions

--- start.sh
+++ start.sh
@@ -27,6 +27,23 @@ if [[ "$(uname)" == "Linux" ]]; then
         yes | pip install flask-socketio
         yes | pip install simple-websocket
         python3 app.py 1234
+    elif command -v pacman >/dev/null 2>&1; then
+    echo 
+    echo Welcome to $(grep -oP '^NAME="\K[^"]+' /etc/os-release).
+    echo To run this script, you need to be root.
+    echo
+        # Arch Based
+        sudo pacman -Syu
+        yes | sudo pacman -S python3
+        yes | sudo pacman -S python-pip
+        yes | sudo pacman -S python-virtualenv
+        python3 -m venv venv
+        source venv/bin/activate
+        yes | sudo pacman -S python3
+        yes | pip install flask
+        yes | pip install flask-socketio
+        yes | pip install simple-websocket
+        python3 app.py 1234
     else
         echo "OS not supported"
     fi
