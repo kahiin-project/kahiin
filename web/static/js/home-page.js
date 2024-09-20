c786e4353eb04bfc7de92f94ecfd9412913e95e6
Mathieu_cpd <mathieu_cpd@noreply.codeberg.org>
2024-09-20 22:55:35 +0200

    Leaderboard now centered, add modified answer handles

--- web/static/js/home-page.js
+++ web/static/js/home-page.js
@@ -1,4 +1,5 @@
 const socket = io();
+var answer_list= [];
 var question_number;
 function Submit() {
   const Username = document.getElementById("Username").value;
@@ -6,12 +7,6 @@ function Submit() {
 
   document.getElementById("form").style.display = "none";
   document.getElementById("loader").style.display = "block";
-
-  socket.on("error", (res) => {
-    alert(res);
-    document.getElementById("form").style.display = "block";
-    document.getElementById("loader").style.display = "none";
-  });
 }
 
 function Game() {
@@ -19,12 +14,25 @@ function Game() {
     document.getElementById("buttons").style.display = "block";
     question_number = res["question_number"]
   });
+  socket.on("questionEnd", (res) => {
+    answer_list = [];
+  });
+  socket.on("error", (res) => {
+    alert(res);
+    document.getElementById("form").style.display = "block";
+    document.getElementById("loader").style.display = "none";
+  });
 }
 
-function sendAnswer(button) {
-  if (button in ["a", "b", "c", "d"]) {
-    emit("sendAnswer", {"button":button, "question_number": question_number});
+function sendAnswer(answer) {
+  if (["a", "b", "c", "d"].includes(answer)) {
+    if (answer_list.includes(answer)) {
+      answer_list.splice(answer_list.indexOf(answer), 1);
+    } else { 
+      answer_list.push(answer);
+    socket.emit("sendAnswer", {"answers":answer_list, "question_number": question_number});}
   } else {
-    alert("Erreur au niveau de la reponse, veuillez réesayer");
+    socket.alert("Erreur au niveau de la reponse, veuillez réesayer");
   }
 };
+
