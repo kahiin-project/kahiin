f27a7e898452c5ec2db6510fdcb676b06dcb409b
tristan-gscn <tristan.gscn@gmail.com>
2024-09-25 09:39:42 +0200

    Manage shown questions on board

--- web/static/js/board-page.js
+++ web/static/js/board-page.js
@@ -56,7 +56,7 @@ function Display() {
   });
 
   socket.on("questionStart", (res) => {
-    document.getElementById("question").style.display = "block";
+    document.getElementById("question").style.bottom = "340px";
     document.getElementById("question").style.opacity = 1;
     document.getElementById("question_number").style.display = "block";
     document.getElementById("question_number").style.opacity = 1;
@@ -64,6 +64,10 @@ function Display() {
     document.getElementById("timer").style.opacity = 1;
     document.getElementById("timerbar").style.display = "block";
     document.getElementById("timerbar").style.opacity = 1;
+    document.getElementById("answers_div").style.display = "block";
+    document.getElementById("answers_div").style.opacity = 1;
+    document.getElementById("answers_div").style.display = "block";
+    document.getElementById("answers_div").style.opacity = 1;
     document.getElementById("loader").style.opacity = 0;
     document.getElementById("loader").style.display = "none";
     document.getElementById("loader-text").style.display = "none";
@@ -80,6 +84,79 @@ function Display() {
           {left: "\$$", right: "\$$", display: true}
       ]
     });
+
+    document.getElementById('answers_div').style.display = "block";
+    switch (res["question_possible_answers"].length) {
+      case 2:
+        document.getElementById('answers_div').innerHTML = `
+          <button class="answer-block" style="background: #99eeff; top: 0; left: 0; height: 200px;">
+            ${res["question_possible_answers"][0]}
+            <svg width="40" height="40" style="top: 80px;">
+              <rect width="40" height="40" fill="white" />
+            </svg>
+          </button>
+          <button class="answer-block" style="background: #BC5BD9; top: 0; left: calc(50% + 10px); height: 200px;">
+            ${res["question_possible_answers"][1]}
+            <svg width="40" height="40" style="top: 80px;">
+              <circle cx="20" cy="20" r="20" fill="white" />
+            </svg>
+          </button>
+        `;
+        break;
+      case 3:
+        document.getElementById('answers_div').innerHTML = `
+          <button class="answer-block" style="background: #99eeff; top: 0; left: 0;">
+            ${res["question_possible_answers"][0]}
+            <svg width="40" height="40">
+              <rect width="40" height="40" fill="white" />
+            </svg>
+          </button>
+          <button class="answer-block" style="background: #ABD95B; top: 0; left: calc(50% + 10px);">
+            ${res["question_possible_answers"][1]}
+            <svg width="40" height="40">
+              <circle cx="20" cy="20" r="20" fill="white" />
+            </svg>
+          </button>
+          <button class="answer-block" style="background: #BC5BD9; top: 110px; left: 0;">
+            ${res["question_possible_answers"][2]}
+            <svg width="40" height="40">
+              <polygon points="20,2 38,38 2,38" fill="white" />
+            </svg>
+          </button>
+          <button class="answer-block" style="background: #DFE6E9; top: 110px; left: calc(50% + 10px);"></button>
+        `;
+        break;
+      case 4:
+        document.getElementById('answers_div').innerHTML = `
+          <button class="answer-block" style="background: #99eeff; top: 0; left: 0;">
+            ${res["question_possible_answers"][0]}
+            <svg width="40" height="40">
+              <rect width="40" height="40" fill="white" />
+            </svg>
+          </button>
+          <button class="answer-block" style="background: #ABD95B; top: 0; left: calc(50% + 10px);">
+            ${res["question_possible_answers"][1]}
+            <svg width="40" height="40">
+              <circle cx="20" cy="20" r="20" fill="white" />
+            </svg>
+          </button>
+          <button class="answer-block" style="background: #BC5BD9; top: 110px; left: 0;">
+            ${res["question_possible_answers"][2]}
+            <svg width="40" height="40">
+              <polygon points="20,2 38,38 2,38" fill="white" />
+            </svg>
+          </button>
+          <button class="answer-block" style="background: #D9955B; top: 110px; left: calc(50% + 10px);">
+            ${res["question_possible_answers"][3]}
+            <svg width="40" height="40">
+              <polygon points="20,2 24.4,14 39.2,14 27.2,22.8 31.6,36 20,28 8.4,36 12.8,22.8 0.8,14 15.6,14" fill="white" />
+            </svg>
+          </button>
+        `;
+        break;
+      default:
+        console.log("Invalid data incoming");
+    }
 
     document.getElementById("timer").innerText = "0";
     document.getElementById("question_number").innerText = `Question ${res["question_number"]}/${res["question_count"]}`;
@@ -120,13 +197,14 @@ function Display() {
 
   socket.on("questionEnd", (res) => {
     document.getElementById("question").style.opacity = 0;
-    document.getElementById("question").style.display = "none";
     document.getElementById("question_number").style.opacity = 0;
     document.getElementById("question_number").style.display = "none";
     document.getElementById("timer").style.opacity = 0;
     document.getElementById("timer").style.display = "none";
     document.getElementById("timerbar").style.opacity = 0;
     document.getElementById("timerbar").style.display = "none";
+    document.getElementById("answers_div").style.opacity = 0;
+    document.getElementById("answers_div").style.display = "none";
     document.getElementById("loader").style.opacity = 1;
     document.getElementById("loader").style.display = "block";
     document.getElementById("loader-text").style.display = "block";
