1450318d1c5a13a4e991192f0f9471b52e01d01d
Mathieu_cpd <mathieu_cpd@noreply.codeberg.org>
2024-09-26 09:13:42 +0200

    Type question handler Answer sender

--- web/static/js/home-page.js
+++ web/static/js/home-page.js
@@ -9,35 +9,38 @@ function Submit() {
   document.getElementById("loader").style.display = "block";
   document.getElementById("loader-text").style.display = "block";
 }
-
 function Game() {
   socket.on("questionStart", (res) => {
     question_number = res["question_number"]
-    if (res["question_type"] == "mqc") {
-      document.getElementById("buttons").style.display = "block";
-      res["question_possible_answers"].forEach(answer => {
-        document.getElementById("button_"+answer).onclick = function() {editAnswer(res[answer])}
-        document.getElementById("button_"+answer).display = "block";
-      document.getElementById("send").style.display = "block";
-      });
-
+    const possible_answer = res["question_possible_answers"];
+    if (res["question_type"] == "mcq") {
+      for (let i = 0; i < possible_answer.length; i++) {
+        document.getElementById("buttons").style.display = "block";
+        console.log(res[possible_answer[i]]);
+        document.getElementById(`button_${i}`).onclick = function() {editAnswer(possible_answer[i])}
+        document.getElementById(`button_${i}`).innerHTML = possible_answer[i];
+        document.getElementById(`button_${i}`).style.display = "block";
+        document.getElementById("send_button").style.display = "block";
+      }
     } else if (res["question_type"] == "uniqueanswer") {
       document.getElementById("buttons").style.display = "block";
-      res["question_possible_answers"].forEach(answer => {
-        document.getElementById("button_"+answer).display = "block";
-      });
-    }   
-                                
+      for (let i = 0; i < possible_answer.length; i++) {
+        console.log(possible_answer[i]);
+        document.getElementById(`button_${i}`).onclick = function() {sendAnswer(possible_answer[i])}
+        document.getElementById(`button_${i}`).innerHTML = possible_answer[i];
+        document.getElementById(`button_${i}`).style.display = "block";
+      }
+    }
   });
+}
 
   socket.on("questionEnd", (res) => {
     answer_list = [];
-    document.getElementById("buttons").style.display = "none";
-    document.getElementById("send").style.display = "none";
-    buttons = ["a", "b", "c", "d"];
-    buttons.array.forEach(button => {
-      document.getElementById("button_"+button).onclick = function() {sendAnswer(button)};
-    });
+    document.getElementById("send_button").style.display = "none";
+    for (let i = 0; i < 4; i++) {
+      document.getElementById(`button_${i}`).style.display = "none";
+      document.getElementById(`button_${i}`).onclick = null;
+    }
   });
 
   socket.on("error", (res) => {
@@ -46,27 +49,33 @@ function Game() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("loader-text").style.display = "none";
   });
-}
 
 function sendAnswer(answer) {
-  if (["a", "b", "c", "d"].includes(answer)) {
-    if (answer_list.includes(answer)) {
-      answer_list.splice(answer_list.indexOf(answer), 1);
-    } else { 
-      answer_list.push(answer);
-    socket.emit("sendAnswer", {"answers":answer_list, "question_number": question_number});}
-  } else {
-    socket.alert("Erreur au niveau de la reponse, veuillez réesayer");
+  console.log(answer);
+  socket.emit("sendAnswer", {"answers":[answer], "question_number": question_number});
+  document.getElementById("send_button").style.display = "none";
+  document.getElementById("buttons").style.display = "none";
+  for (let i = 0; i < 4; i++) {
+    document.getElementById(`button_${i}`).style.display = "none";
+    document.getElementById(`button_${i}`).onclick = null;
   }
+  // if (["a", "b", "c", "d"].includes(answer)) {
+  //   if (answer_list.includes(answer)) {
+  //     answer_list.splice(answer_list.indexOf(answer), 1);
+  //   } else { 
+  //     answer_list.push(answer);
+  //   socket.emit("sendAnswer", {"answers":answer_list, "question_number": question_number});}
+  // } else {
+  //   socket.alert("Erreur au niveau de la reponse, veuillez réesayer");
+  // }
 };
 
 function editAnswer(answer) {
-  if (["a", "b", "c", "d"].includes(answer)) {
-    if (answer_list.includes(answer)) {
-      answer_list.splice(answer_list.indexOf(answer), 1);
-    } else { 
-      answer_list.push(answer);
-    }}};
+  if (answer_list.includes(answer)) {
+    answer_list.splice(answer_list.indexOf(answer), 1);
+  } else { 
+    answer_list.push(answer);
+  }};
 
 
 function sendMQC() {
