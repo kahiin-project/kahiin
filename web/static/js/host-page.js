caeb2e53086db25b50e4f426cacd373d63ccd4f0
Mathieu_cpd <mathieu.chapouand@gmail.com>
2024-10-02 09:44:39 +0200

    Add error html to host

--- web/static/js/host-page.js
+++ web/static/js/host-page.js
@@ -6,9 +6,12 @@ function Submit() {
     document.getElementById("start").style.display = "block"
     socket.emit("hostConnect", passcode);
     socket.on("error", (res) => {
-      alert(res);
+      document.getElementById("passcode").value = "";
+      document.getElementById("error").style.display = "block";
+      document.getElementById("error").innerHTML = res;
       document.getElementById("form").style.display = "block";
-      document.getElementById("start").style.display = "none"
+      document.getElementById("start").style.display = "none";
+      document.getElementById("next").style.display = "none";
     });
 }
 
