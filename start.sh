61d14995717cdcdf993fb6dc4499a33a8584b9a0
Mathieu_cpd <mathieu_cpd@noreply.codeberg.org>
2025-03-23 15:30:59 +0100

    Remove verbose

--- start.sh
+++ start.sh
@@ -1,43 +1,31 @@
 #!/bin/bash
 
-# Detect operating system
 if [[ "$(uname)" == "Linux" ]]; then
     if [[ -f /data/data/com.termux/files/usr/bin/bash ]]; then
-        # Termux
-        yes | pkg update
-        yes | pkg upgrade
-        yes | pkg install python3
-        yes | pip install -r requirements.txt
+        echo "Detected Termux environment. Setting up the environment..."
+        pkg update -q
+        pkg upgrade -q
+        pkg install -y -q python3
+        pip install -r requirements.txt -q
+        echo "Starting the Kahiin server..."
         python3 app.py 1234
     elif command -v apt >/dev/null 2>&1; then
-        # Debian Based
-        echo 
-        echo Welcome to $(grep -oP '^NAME="\K[^"]+' /etc/os-release).
-        echo To run this script, you need to be root.
-        echo
-        yes | sudo apt update
-        yes | sudo apt install python3 
-        yes | sudo apt install python3-pip
-        yes | sudo apt install python$(python3 -V 2>&1 | awk -F'[ .]' '{print $2"."$3}')-venv
+        echo "Detected Debian based system. Setting up the environment..."
+        sudo apt update -qq
+        sudo apt install -y -qq python3 python3-pip python3-venv
         python3 -m venv venv
         source venv/bin/activate
-        yes | sudo apt install python3
-        yes | pip install -r requirements.txt
+        pip install -r requirements.txt -q
+        echo "Starting the Kahiin server..."
         python3 app.py 1234
     elif command -v pacman >/dev/null 2>&1; then
-    echo 
-    echo Welcome to $(grep -oP '^NAME="\K[^"]+' /etc/os-release).
-    echo To run this script, you need to be root.
-    echo
-        # Arch Based
-        yes | sudo pacman -Syu
-        yes | sudo pacman -S python3
-        yes | sudo pacman -S python-pip
-        yes | sudo pacman -S python-virtualenv
+        echo "Detected Arch based system. Setting up the environment..."
+        sudo pacman -Syu --noconfirm > /dev/null
+        sudo pacman -S --noconfirm python3 python-pip python-virtualenv > /dev/null
         python3 -m venv venv
         source venv/bin/activate
-        yes | sudo pacman -S python3
-        yes | pip install -r requirements.txt
+        pip install -r requirements.txt -q
+        echo "Starting the Kahiin server..."
         python3 app.py 1234
     else
         echo "OS not supported"
